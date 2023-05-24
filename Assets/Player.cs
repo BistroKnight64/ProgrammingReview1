fc38fbb96f8a508cb7a69d7ed5dd6a92405e5e2a
mrKrabsararararararar <118307230+BistroKnight64@users.noreply.github.com>
2023-05-24 11:48:45 -0700

    Iteration2

---
+++ Assets/Player.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float HorizInput;
+    public float VertInput;
+    public float Speed;
+    public bool Canlose;
+    public Vector2 StupidC;
+
+    public SpriteRenderer amogus;
+    public Sprite Deadstuff;
+    public Sprite Alivestuff;
+    public GameManager Man;
+    public GameObject Stuff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        amogus = gameObject.GetComponent<SpriteRenderer>();
+        Man = Stuff.GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizInput = Input.GetAxis("Horizontal");
+        VertInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.right * Speed * HorizInput * Time.deltaTime);
+        transform.Translate(Vector2.up * Speed * VertInput * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Speed = 0;
+            amogus.sprite = Deadstuff;
+            if(Canlose == true)
+            {
+            Man.playLif--;
+                Man.Updatelife();
+                Canlose = false;
+            }
+            StartCoroutine(CoolDown());
+        }
+    }
+
+    IEnumerator CoolDown()
+    {
+        yield return new WaitForSeconds(3);
+
+     Teleport();
+        Canlose = true;
+        Speed = 5;
+        amogus.sprite = Alivestuff;
+    }
+
+    void Teleport()
+    {
+        //  Stupid
+        gameObject.transform.position = new Vector2(Random.Range(6, 0), Random.Range(6, 0));
+    }
+}
